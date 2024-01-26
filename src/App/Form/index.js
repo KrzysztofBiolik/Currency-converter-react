b64f68b32d67352d93d0d20fd5d9f6eb84c94672
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-01-26 12:12:05 +0100

    Added a selection of different currencies

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,39 +1,50 @@
 import { Result } from "./Result";
+import { currencies } from "../currencies";
 import "./style.css";
 
-export const Form = () => (
+export const Form = () => {
 
-    <form className="form" >
-        <h1 className="header">
-            Kalkulator walut
-        </h1>
-        <p>
-            <label>
-                <span className="form__labelText">
-                    Kwota w zł*:
-                </span>
-                <input className="form__field"
-                    type="number"
-                    required
-                    step="0.01"
-                    placeholder="Wpisz kwotę w zł"
-                />
-            </label>
-        </p>
-        <p>
-            <label>
-                <span className="form__labelText">
-                    Waluta:
-                </span>
-                <select className="form__field"
-                    value
-                >
-                </select>
-            </label>
-        </p >
-        <p>
-            <button className="form__button">Przelicz!</button>
-        </p>
-        <Result />
-    </form >
-);
+    return (
+        <form className="form" >
+            <h1 className="header">
+                Kalkulator walut
+            </h1>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Kwota w zł*:
+                    </span>
+                    <input className="form__field"
+                        type="number"
+                        required
+                        step="0.01"
+                        placeholder="Wpisz kwotę w zł"
+                    />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Waluta:
+                    </span>
+                    <select
+                        className="form__field"
+                    >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.name}
+                            </option>
+                        )))}
+                    </select>
+                </label>
+            </p >
+            <p>
+                <button className="form__button">Przelicz!</button>
+            </p>
+            <Result />
+        </form >
+    )
+};
---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.15
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.58,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.19
+    },
+];
