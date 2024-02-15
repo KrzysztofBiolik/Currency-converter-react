6650c7259914a16ecfc172a4ea0dd2f1ad4be8a8
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-02-15 14:05:55 +0100

    Added a message for entering a negative initial value

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,16 +1,28 @@
 import "./style.css";
 
-export const Result = ({result}) => (
-    <p className="result">
-        {result !== undefined && (
-            <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                {" "}
 
-                <strong>
-                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                </strong>
-            </>
-        )}
-    </p>
-);
+export const Result = ({ result }) => {
+    if (!result) {
+        return null;
+    }
+
+    return (
+        <container>
+            {result.sourceAmount < 0 ?
+                (
+                    "Nie można przeliczać ujemnych kwot"
+                )
+                :
+                (
+                    <>
+                        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+                        {" "}
+                        <strong>
+                            {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                        </strong>
+                    </>
+                )}
+        </container>
+    );
+};
+
